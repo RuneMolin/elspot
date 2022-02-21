b1389de48634439683b15dc0eb03b186aab6eb8c
Rune Molin <rmo@nine.dk>
2022-02-21 20:27:18 +0100

    pwa attempt

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,11 @@ module.exports = {
       title: "El prisen",
     },
   },
+  pwa: {
+    themeColor: "#17a2b8",
+    msTileColor: "#000000",
+    iconPaths: {
+      msTileImage: "img/icons/mstile-150x150.png",
+    },
+  },
 };
